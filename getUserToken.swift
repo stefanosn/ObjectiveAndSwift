b92d70310e834e1906ff1b1346ebdbf920211ebc
stefanosn <stefanos.neofitidis@weare8.com>
2022-01-01 03:36:44 +0200

    functionality extracted to methods

--- getUserToken.swift
+++ getUserToken.swift
@@ -13,9 +13,9 @@ import FoundationNetworking
 @objc public class ObjectiveAndSwift:NSObject{
     //start of class
     @objc func getToken() -> String{
-let semaphore = DispatchSemaphore (value: 0)
 
-let parameters = [
+
+        let parameters = [
   [
     "key": "grant_type",
     "value": "password",
@@ -46,64 +46,80 @@ let parameters = [
     "value": "openid users.api campaigns.api offline_access",
     "type": "text"
   ]] as [[String : Any]]
-        var responseData = ""
-let boundary = "Boundary-\(UUID().uuidString)"
-var body = ""
-//var error: Error? = nil
-for param in parameters {
-  if param["disabled"] == nil {
-    let paramName = param["key"]!
-    body += "--\(boundary)\r\n"
-    body += "Content-Disposition:form-data; name=\"\(paramName)\""
-    if param["contentType"] != nil {
-      body += "\r\nContent-Type: \(param["contentType"] as! String)"
-    }
-    let paramType = param["type"] as! String
-    if paramType == "text" {
-      let paramValue = param["value"] as! String
-      body += "\r\n\r\n\(paramValue)\r\n"
-    } else {
-      let paramSrc = param["src"] as! String
        
-        do {
-            let fileData = try NSData(contentsOfFile:paramSrc, options:[]) as Data
-            
-      
-//        throws {
-//      fileData = try NSData(contentsOfFile:paramSrc, options:[]) as Data
-//        }
-      let fileContent = String(data: fileData, encoding: .utf8)!
-      body += "; filename=\"\(paramSrc)\"\r\n"
-        + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
-        } catch {
-            print("Unexpected error")
-        }
-    }
-  }
-}
-body += "--\(boundary)--\r\n";
-let postData = body.data(using: .utf8)
+        var body = ""
+        let boundary = "Boundary-\(UUID().uuidString)"
+        body=setBodyOfRequest(parameters1:parameters,boundary1:boundary)
+        
+        let postData = body
+        let responseData = setURLQuery(postData1: postData, boundary1: boundary)
 
-var request = URLRequest(url: URL(string: "https://api-uat.test.aws.the8app.com/eightauthapi/connect/token")!,timeoutInterval: Double.infinity)
-request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
-request.httpMethod = "POST"
-request.httpBody = postData
-
-let task = URLSession.shared.dataTask(with: request) { data, response, error in
-  guard let data = data else {
-    print(String(describing: error))
-    semaphore.signal()
-    return
-  }
-  print(String(data: data, encoding: .utf8)!)
-    responseData = String(data: data, encoding: .utf8)!
-  semaphore.signal()
-}
-
-task.resume()
-semaphore.wait()
         return responseData;
 }
+    
+    func setURLQuery(postData1:String,boundary1:String) -> String{
+        let semaphore = DispatchSemaphore (value: 0)
+        
+        var request = URLRequest(url: URL(string: "https://api-uat.test.aws.the8app.com/eightauthapi/connect/token")!,timeoutInterval: Double.infinity)
+        request.addValue("multipart/form-data; boundary=\(boundary1)", forHTTPHeaderField: "Content-Type")
+
+        request.httpMethod = "POST"
+        request.httpBody = postData1.data(using: .utf8)
+
+        var response1 = ""
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            semaphore.signal()
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+            response1 = String(data: data, encoding: .utf8)!
+          semaphore.signal()
+        }
+
+        task.resume()
+        semaphore.wait()
+        return response1
+    }
+    
+    
+    func setBodyOfRequest(parameters1:[[String : Any]],boundary1:String) -> String{
+        var body = ""
+        //var error: Error? = nil
+        for param in parameters1 {
+          if param["disabled"] == nil{
+            let paramName = param["key"]!
+            body += "--\(boundary1)\r\n"
+            body += "Content-Disposition:form-data; name=\"\(paramName)\""
+            if param["contentType"] != nil {
+              body += "\r\nContent-Type: \(param["contentType"] as! String)"
+            }
+            let paramType = param["type"] as! String
+            if paramType == "text" {
+              let paramValue = param["value"] as! String
+              body += "\r\n\r\n\(paramValue)\r\n"
+            } else {
+              let paramSrc = param["src"] as! String
+               
+                do {
+                    let fileData = try NSData(contentsOfFile:paramSrc, options:[]) as Data
+                    
+              
+        //        throws {
+        //      fileData = try NSData(contentsOfFile:paramSrc, options:[]) as Data
+        //        }
+              let fileContent = String(data: fileData, encoding: .utf8)!
+              body += "; filename=\"\(paramSrc)\"\r\n"
+                + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
+                } catch {
+                    print("Unexpected error")
+                }
+            }
+          }
+        }
+        body += "--\(boundary1)--\r\n";
+        return body
+    }
     //end of class
 }
